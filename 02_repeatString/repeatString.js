d0937fa9fca44adceba81a69acc4e8c9bd1e0fa5
RonnieMu34 <ronnie34mu@gmail.com>
2022-07-06 13:58:37 -0400

    Complete exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(userWord, numTimes) {
+    let concatStr = "";
+    if (numTimes < 0) {
+        return "ERROR";
+    } else {
+        for (let i = 0; i < numTimes; i++) {
+            concatStr += userWord;
+        }
+        return concatStr;
+    }
 };
 
 // Do not edit below this line
