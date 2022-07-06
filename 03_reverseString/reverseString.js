fdda6b6315b0ac8daf83fc0ecca337df8a6da389
RonnieMu34 <ronnie34mu@gmail.com>
2022-07-06 14:04:23 -0400

    Complete exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(originStr) {
+    reversedStr = "";
+for (let i = originStr.length-1; i >= 0; i--) {
+    reversedStr += originStr[i];
+}
+return reversedStr;
 };
 
 // Do not edit below this line
