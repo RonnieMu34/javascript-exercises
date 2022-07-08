7ac1accb70b8447933fb4f158183a23b1ac41c6c
RonnieMu34 <ronnie34mu@gmail.com>
2022-07-08 12:28:40 -0400

    Complete Exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(temperature) {
+  rawTemp = ((temperature - 32) * (5/9));
+  return parseFloat(rawTemp.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(temperature) {
+  rawTemp = ((temperature * (9/5)) + 32);
+  return parseFloat(rawTemp.toFixed(1));
 };
 
 // Do not edit below this line
