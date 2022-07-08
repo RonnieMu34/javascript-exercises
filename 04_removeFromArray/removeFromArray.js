b0ffcbd9f348dc2c58fe452212121ca2082fcf7b
RonnieMu34 <ronnie34mu@gmail.com>
2022-07-08 11:46:55 -0400

    Complete exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(originArray, ...theTargets) {
+    for (const target of theTargets) {
+        if (!originArray.includes(target)) {
+            continue;
+        } else {
+            originArray.splice(originArray.indexOf(target), 1);
+        }
+    }
+    return originArray;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
