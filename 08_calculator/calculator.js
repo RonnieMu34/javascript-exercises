b47c559cbafc4141568a17386cae9a6dee2834d4
RonnieMu34 <ronnie34mu@gmail.com>
2022-08-25 13:07:24 -0400

    Complete exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,51 @@
-const add = function() {
+const add = function(a, b) {
+	return a + b;
+};
+
+const subtract = function(a, b) {
+  return a - b;
 	
 };
 
-const subtract = function() {
-	
+const sum = function(array) {
+  let currSum = 0;
+	for (let i = 0; i < array.length; i++) {
+    currSum = currSum + array[i];
+  }
+  return currSum;
 };
 
-const sum = function() {
-	
+const multiply = function(array) {
+  let currProd = 1;
+	for (let i = 0; i < array.length; i++) {
+    currProd = currProd * array[i];
+  }
+  return currProd;
 };
 
-const multiply = function() {
-
+const power = function(a, b) {
+  let currProd = 1;
+	if (b == 0) {
+    return 1;
+  } else {
+    for (let i = 1; i <= b; i++) {
+      currProd = currProd * a;
+    }
+    return currProd;
+  }
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(a) {
+  let prod = 1;
+  if (a == 0) {
+    return 1;
+  } else {
+    for (let i = 1; i <= a; i++) {
+      prod *= i;
+    }
+    return prod;
+  }
+    
 };
 
 // Do not edit below this line
