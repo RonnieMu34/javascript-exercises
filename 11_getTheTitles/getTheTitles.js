732bd3c5d819b0eddcb98d4abd5b095aa6bd4c48
RonnieMu34 <ronnie34mu@gmail.com>
2022-08-26 13:07:06 -0400

    Complete Exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,4 +1,8 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    const titleArr = books.map(function(book) {
+        return book.title;
+    });
+    return titleArr;
 
 };
 
