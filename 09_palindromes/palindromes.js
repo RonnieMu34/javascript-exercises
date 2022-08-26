1a182632ce96d79f0ba77c73bb10e7c5f2f8c06c
RonnieMu34 <ronnie34mu@gmail.com>
2022-08-26 10:54:53 -0400

    Complete exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,20 @@
-const palindromes = function () {
+const palindromes = function (userStr) {
+    userStr = userStr.toLowerCase();
+    let reverseStr = [];
+    userStr = userStr.split("");
+    rawStr = userStr.filter(function(strChar) {
+        return strChar.match(/[a-z]/i);
+    })
+    for (let i = rawStr.length - 1; i >= 0; i--) {
+        reverseStr.push(rawStr[i]);
+    }
+    for (let j = 0; j < rawStr.length; j++) {
+        if (rawStr[j] != reverseStr[j]) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 
 };
 
