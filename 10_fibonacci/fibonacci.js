c5f6514178411870dd8e4bbf78e58d788949246d
RonnieMu34 <ronnie34mu@gmail.com>
2022-08-26 12:57:49 -0400

    Complete exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(member) {
+    if (member < 0) {
+        return "OOPS";
+    } else {
+        let fibArr = [1];
+    num1 = 0;
+    num2 = 1;
 
+    for (let i = 0; i < member - 1; i++) {
+        fibArr.push(num1 + num2);
+        num2 = fibArr[i + 1];
+        num1 = fibArr[i];
+    }
+    return fibArr[member - 1];
+    }
 };
 
 // Do not edit below this line
