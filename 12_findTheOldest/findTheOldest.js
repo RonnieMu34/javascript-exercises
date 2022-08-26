2835722e60c1a5fded1e682d0be397a398059d81
RonnieMu34 <ronnie34mu@gmail.com>
2022-08-26 13:40:52 -0400

    Complete exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+    // const died = people.filter(function(person) {
+    //     if (person.yearOfDeath != undefined) {
+    //         return true;
+    //     }
+    // });
+
+    for (let i = 0; i < people.length; i++) {
+        if (people[i].yearOfDeath == undefined) {
+            people[i].yearOfDeath = ((new Date()).getFullYear());
+        }
+    }
+
+    const oldest = people.sort(function(a, b) {
+        if ((a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth)) {
+            return 1;
+        } else {
+            return -1;
+        }
+    });
+
+    return oldest[oldest.length - 1];
 };
 
 // Do not edit below this line
